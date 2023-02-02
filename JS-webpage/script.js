6acb492f80c08ecab9401110cd83a69e128a710d
craig91 <craigdejean1@gmail.com>
2023-02-01 20:42:55 -0500

    Added multidimensional array

--- JS-webpage/script.js
+++ JS-webpage/script.js
@@ -41,3 +41,9 @@ function getFullName(item) {
 }
 
 persons.map(getFullName);
+
+
+// return orange from this arrray
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(array2[1][1]);
+
