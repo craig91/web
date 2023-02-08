bddc9cc7ce55995ebd0078cb90ffdb4c9ea5a0c9
craig91 <craigdejean1@gmail.com>
2023-02-08 16:24:52 -0500

    Added advance object code

--- JS-webpage/script.js
+++ JS-webpage/script.js
@@ -480,3 +480,27 @@ const itemOwner = array.map((user) => {
 })
 
 console.log(itemOwner)
+
+class Player {
+  constructor(name, type) {
+      console.log('player',this)
+      this.name = name;
+      this.type = type;
+  }
+  introduce() {
+      console.log(`Hi, I am ${this.name}, I'm a ${this.type}`)
+  }
+}
+
+class Wizard extends Player {
+  constructor(name, type) {
+    console.log('wizard', this)
+    super(name, type)
+  }
+  play() {
+    console.log(`weee i'm a ${this.type}`);
+  }
+}
+
+const wizard1 = new Wizard('Shelly', 'Healer');
+const wizard2 = new Wizard('Shawn', 'Dark Magic')
