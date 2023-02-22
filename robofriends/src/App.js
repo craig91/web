b1c147602fa2750fd4d9a2c29f731f747e4c7174
craig91 <craigdejean1@gmail.com>
2023-02-21 19:30:12 -0500

    added testing component

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './Test-Components/test01';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React
+          <Test />
         </a>
       </header>
     </div>
---
+++ robofriends/src/Test-Components/test01.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Test() {
+    return(
+        <div className="Test">
+            <h1>This is a test component</h1>
+        </div>
+    );
+}
+
+export default Test;
