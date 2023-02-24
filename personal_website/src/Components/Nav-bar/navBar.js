08cce5c15288cd4302d4a110ee336ee0713aa1f6
craig91 <craigdejean1@gmail.com>
2023-02-23 19:56:58 -0500

    added navigation bar component with styles

--- personal_website/src/Components/Nav-bar/navBar.js
+++ personal_website/src/Components/Nav-bar/navBar.js
@@ -1,7 +1,16 @@
+// import './Components/Nav-bar/navBarStyle.css';
 function Nav() {
     return(
         <div>
-            Nav Bar test
+            <header className="header">
+                <h1 className="logo"><a href="#"></a></h1>
+                <ul className="main-nav">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Contact</li>
+                </ul>
+            </header>
         </div>
     )
 }
