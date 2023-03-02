e35aa7186797a01cd2b52beb717223e221cebbe0
craig91 <craigdejean1@gmail.com>
2023-03-01 19:53:06 -0500

    added new practice file

---
+++ JS-webpage/practice.js
@@ -0,0 +1,16 @@
+// Eloquent JS chapter 2
+let caught = 5 * 5  // <-- this is called binding, 
+console.log(caught)
+
+let ten = 10
+console.log(ten * ten)
+
+let mood = "light";
+console.log(mood);
+
+mood = "dark";
+console.log(mood);
+
+let luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log("$" + luigisDebt);
