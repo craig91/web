e352b692a3dd91b23225cb229e46f2988497c70b
craig91 <craigdejean1@gmail.com>
2023-06-07 13:14:10 +0900

    added spread operator to create new array

--- node-2/js-refresh2.js
+++ node-2/js-refresh2.js
@@ -16,6 +16,8 @@ for (let hobby of hobbies) {
 // console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
 // console.log(hobbies);
 
-hobbies.push('Programming')
-console.log(hobbies)
+const copiedArray = [...hobbies]
+console.log(copiedArray)
+
+
 
