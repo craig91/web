13409f954e3e9f11bb80189add1a4cec38ffe700
craig91 <craigdejean1@gmail.com>
2023-06-06 23:43:18 -0400

    Changed mutable variables to immutable variables

---
+++ node-2/js-refresh.js
@@ -0,0 +1,16 @@
+const name = 'Craig';
+let age = 31;
+const hasHobbies = true;
+
+
+age = 30;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+    return 'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby;
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
+
+
+
+
