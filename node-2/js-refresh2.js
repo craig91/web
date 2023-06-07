c4d8b65eb0c6cf7987ce9af5b791cdccd9c7cdcc
craig91 <craigdejean1@gmail.com>
2023-06-07 13:09:55 +0900

    edited array with push method

--- node-2/js-refresh2.js
+++ node-2/js-refresh2.js
@@ -13,6 +13,9 @@ for (let hobby of hobbies) {
     console.log(hobby)
 }
 
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+hobbies.push('Programming')
 console.log(hobbies)
 
