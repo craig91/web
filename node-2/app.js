008616da458e629cd4a71c457ab38951f9465e51
craig91 <craigdejean1@gmail.com>
2023-06-08 10:58:09 +0900

    Added new server

---
+++ node-2/app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+
+// function rqListener(req, res) {
+    
+// }
+// http.createServer(rqListener);
+
+// http.createServer(function(req, res) {
+
+// });
+
+const server = http.createServer((req,res) => {
+    console.log(req);
+});
+
+server.listen(3000);
