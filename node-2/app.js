a49d13772b16be048b79e18b8c05448328a558d7
craig91 <craigdejean1@gmail.com>
2023-06-24 11:21:58 +0900

    Added 302 redirect to conditional

--- node-2/app.js
+++ node-2/app.js
@@ -1,9 +1,12 @@
 const http = require('http');
+const fs = require('fs');
 
 
 const server = http.createServer((req,res) => {
     console.log(req.url, req.method, req.headers);
     const url = req.url;
+    const method = req.method;
+
     if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter message</title></head>');
@@ -11,6 +14,15 @@ const server = http.createServer((req,res) => {
     res.write('</html>');
     return res.end()
     }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+
+    }
+    
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
