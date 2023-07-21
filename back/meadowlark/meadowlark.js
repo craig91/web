3f418c6301895a5829f956cc8731e5c3bdbb741a
craig91 <craigdejean1@gmail.com>
2023-07-21 14:13:24 +0900

    installed jest and added unit test to __tests__ directory

--- back/meadowlark/meadowlark.js
+++ back/meadowlark/meadowlark.js
@@ -2,16 +2,10 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const app = express();
 const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 const port = process.env.port  || 3000;
 
 
-// const fortunes = [
-//     "Conquer your fears or they will conquer you.",
-//     "Rivers need springs",
-//     "Do not fear what you don't know",
-//     "You will have a pleasant surprise",
-//     "Whenever possible, keep it simple"
-// ];
 
 
 app.engine('handlebars', engine({
@@ -21,25 +15,15 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 
-app.get('/', (req, res) => res.render('home'))
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/', handlers.home) 
+app.get('/about', handlers.about)
 
 
 app.use(express.static(__dirname + './public'));
 
 
-app.use((req, res) => {
-    res.status(404);
-    res.render('404');
-})
-
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500);
-    res.render('500')
-})
+app.use(handlers.notFound)
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` + 
