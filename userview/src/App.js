5a2855018a3808cd69af924a3898b17d41080198
craig91 <craigdejean1@gmail.com>
2023-10-18 21:51:42 -0400

    initial commit: new web app

---
+++ userview/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './components/button';
+import Button from './components/button';
+import Nav from './components/NavBar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ userview/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Nav() {
+    return <nav className="nav-bar">
+        <h2>Title</h2>
+        <ul>
+            <li><a href="#">Create User</a></li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+    </nav>
+}
+
