38431cdecbaeafc183b682b25333d1804ff7d3bb
craig91 <craigdejean1@gmail.com>
2023-12-19 22:50:33 -0500

    Added a practice code file

---
+++ JS_stuff/stuff.js
@@ -0,0 +1,44 @@
+// let caught = 5 * 5;
+
+// let ten = 10;
+// console.log(ten * ten);
+
+// let mood = 'light';
+// console.log(mood);
+
+// mood = 'dark';
+// console.log(mood)
+
+// let luigisDebt = 140;
+// luigisDebt = luigisDebt - 35;
+// console.log("Luigi's Debt: " + "$" + luigisDebt)
+
+// let one = 1, two = 2;
+// console.log(one + two)
+
+// var name = "Ayda"
+// const greeting = "Hello ";
+// console.log(greeting + name);
+
+// console.log(Math.max(2, 4))
+
+// console.log(Math.min(2, 4) + 100);
+
+// let theNumber = Number(prompt("Pick a number"));
+// if (!Number.isNaN(theNumber)) {
+//     console.log("Your number is the square root of " + theNumber * theNumber);
+// }
+
+let number = 0;
+while (number <= 12) {
+    console.log(number);
+    number += 2;
+}
+
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+    result = result * 2;
+    counter = counter + 1;
+}
+console.log(result)
