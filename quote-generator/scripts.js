6eb2959bd1005fc37629b36111f37961055c68d4
craig91 <craigdejean1@gmail.com>
2023-12-20 22:14:22 -0500

    added api call to quotes api

--- quote-generator/scripts.js
+++ quote-generator/scripts.js
@@ -1,3 +1,30 @@
 // Api link1: https://type.fit/api/quotes
 // Api link2: https://jacintodesign.github.io/quotes-api/data/quotes.json
 
+// GLOBAL BINDINGS
+let apiQuotes = [];
+
+newQuote = () => {
+    // picks a random quote from the quotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote)
+    
+}
+
+// Get quotes from API
+
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        // console.log(apiQuotes[12]);
+        newQuote();
+    } catch (error) {
+        // handle error
+    }
+}
+
+// On Load
+
+getQuotes();
