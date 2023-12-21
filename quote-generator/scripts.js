5651e2486dc2c8baa02f2dc93361c98fff885414
craig91 <craigdejean1@gmail.com>
2023-12-20 22:52:56 -0500

    Added functionality to buttons, changed twitter icon to x icon

--- quote-generator/scripts.js
+++ quote-generator/scripts.js
@@ -1,13 +1,38 @@
-// Api link1: https://type.fit/api/quotes
-// Api link2: https://jacintodesign.github.io/quotes-api/data/quotes.json
+// API link1: https://type.fit/api/quotes
+// API link2: https://jacintodesign.github.io/quotes-api/data/quotes.json
+// API link3: https://zenquotes.io/
+
 
 // GLOBAL BINDINGS
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 newQuote = () => {
     // picks a random quote from the quotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote)
+
+    // check if author field is blank then replace with unknown
+
+    if (!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author;
+    }
+    
+    // check quote length for styling
+
+    if (quote.text.length > 50) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
     
 }
 
@@ -18,12 +43,25 @@ async function getQuotes() {
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        // console.log(apiQuotes[12]);
         newQuote();
     } catch (error) {
         // handle error
     }
 }
+
+// tweet quote
+tweetQuote = () => {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+
 
 // On Load
 
