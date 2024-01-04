432e3cb2ead6194ead62c058ea25873023fd05f9
craig91 <craigdejean1@gmail.com>
2024-01-03 20:51:23 -0500

    added key ids to map function

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,19 +1,39 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: {firstName: 'Craig', lastName: "Dejean"},
-      company: 'Microsoft'
+      monsters: [
+        {
+          name: 'Linda',
+          id: 1
+        },
+        {
+          name: 'Frank',
+          id: 2
+        },
+        {
+          name: 'Jacky',
+          id: 3
+        },
+        {
+          name: 'Craig',
+          id: 4
+        }
+      ]
+     
     }
   }
   render() {
     return (
       <div className="App">
-       
+        { // Map creates a returns a new array + also each child needs a key prop
+          this.state.monsters.map((monster) => {
+            return <h1 key={monster.id} >{monster.name}</h1>
+          })
+        }
       </div>
     );
   }
