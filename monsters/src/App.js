9c936215c09c8d1d0d8dacc7d3a134f4d728399d
craig91 <craigdejean1@gmail.com>
2024-01-03 21:46:27 -0500

    Added comments on lifecycle order from state to render

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -2,15 +2,15 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor() {  // 1st to run --> initializes state and its intial value
+    super()  
     this.state = {
       monsters: [],
     }
   }
 
 
-  componentDidMount() {
+  componentDidMount() { // runs 3rd --> the moment the component mounts (from render) the code inside this lifecycle method runs hence the name "component did mount"
       fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then((users) => this.setState(() => {
@@ -21,7 +21,7 @@ class App extends Component {
               }
             ));
   }
-  render() {
+  render() { // Runs 2nd --> deterimines what to show (to the screen) --> renders the initial UI of this component
     return (
       <div className="App">
         { // Map creates a returns a new array + also each child needs a key prop
