e1dc2a3a3c54d2d7998da7357d822aca1a0520b3
craig91 <craigdejean1@gmail.com>
2024-01-04 13:49:07 -0500

    added optimizations to search bar

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -6,20 +6,21 @@ class App extends Component {
     super()  
     this.state = {
       monsters: [],
+      searchField: ''
     }
-    console.log('Constructor');
+    // console.log('Constructor');
   }
 
 
   componentDidMount() { // runs 3rd --> the moment the component mounts (from render) the code inside this lifecycle method runs hence the name "component did mount"
-      console.log('Component Did Mount')
+      // console.log('Component Did Mount')
       fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then((users) => this.setState(() => {
               return {monsters: users}
             },
               () => {
-                console.log(this.state);
+                // console.log(this.state);
               }
             ));
   }
@@ -28,7 +29,12 @@ class App extends Component {
 
   render() {  // Runs 2nd --> deterimines what to show (to the screen) --> renders the initial UI of this component
               // Runs 4th --> now that component did mount ran (state has changed due to setState()) --> render method runs again.
-    console.log('Render method')
+    // console.log('Render method')
+
+    const filteredMonsters = this.state.monsters.filter((monster) => { // <-- filter() gives back a new array, so the original array (in state above) is not modified
+      return monster.name.toLowerCase().includes(this.state.searchField); // <-- returns to me the monster name in lowercase and check if its included in the new array. 
+    });
+
     return (
       <div className="App">
         <input 
@@ -37,21 +43,24 @@ class App extends Component {
           placeholder='Search Monsters' 
           onChange={(event) => 
           {
-            console.log(event.target.value);
-            const searchString = event.target.value.toLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLowerCase().includes(searchString);
-            });
+            // console.log(event.target.value);
+            // console.log({startingArray: this.state.monsters})
+            const searchField = event.target.value.toLowerCase(); // <-- event.target.value refers to the input I type into the search string
+            // const filteredMonsters = this.state.monsters.filter((monster) => { // <-- filter() gives back a new array, so the original array (in state above) is not modified
+            //   return monster.name.toLowerCase().includes(searchString); // <-- returns to me the monster name in lowercase and check if its included in the new array. 
+            // });
 
             this.setState(() => {
-              return { monsters: filteredMonsters }
+              return { searchField };
+            }, () => {
+              // console.log({EndingArray: this.state.monsters})
             })
           }}/>
 
 
         { // Map creates a returns a new array + also each child needs a key prop
-          this.state.monsters.map((monster) => {
-            return <div key={monster.id} ><h1>{monster.name}</h1></div>
+          filteredMonsters.map((monster) => {
+            return <div key={monster.id}><h1>{monster.name}</h1></div>
           })
         }
       </div>
